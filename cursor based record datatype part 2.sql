2267f7a15ffa60e051623b343cdad8b1f81b12ba
Faizan Ahmad <faizanahmad1033@gmail.com>
2022-10-20 12:58:32 +0500

    cursor based record datatype part 2

---
+++ cursor based record datatype part 2.sql
@@ -0,0 +1,14 @@
+declare 
+cursor cur_p is 
+select emp_name,salary from employees 
+where emp_id>100 and emp_id<105;
+v_emp cur_p%rowtype;
+begin 
+open cur_p;
+loop
+fetch cur_p into v_emp;
+exit when cur_p%notfound;
+dbms_output.put_line(v_emp.emp_name||' '||v_emp.salary);
+end loop;
+close cur_p;
+end;
